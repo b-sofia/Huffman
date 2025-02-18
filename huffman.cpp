078a7065591310628abbbe6862fae9391657a9df
b-sofia <borodkina.sofia@yandex.ru>
2025-02-18 14:07:14 +0300

    Delete huffman.cpp

--- huffman.cpp
+++
@@ -1,9 +0,0 @@
-#include "huffman.h"
-Huffman::Huffman() {}
-std::map<char, int> Huffman::countFrequencies(const std::string& data) {
-    std::map<char, int> freqMap;
-    for (char ch : data) {
-        freqMap[ch]++;
-    }
-    return freqMap;
-}
