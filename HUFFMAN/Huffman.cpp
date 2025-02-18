62424a543b9f1c1fec752ec94ebecb28dea0578a
egortr2020 <136753951+egortr2020@users.noreply.github.com>
2025-02-18 14:18:35 +0300

    added countFrequencies, countFrequencies and struct Compare

--- HUFFMAN/Huffman.cpp
+++ HUFFMAN/Huffman.cpp
@@ -1,1 +1,9 @@
-#include "Huffman.h"
+#include "huffman.h"
+Huffman::Huffman() {}
+std::map<char, int> Huffman::countFrequencies(const std::string& data) {
+    std::map<char, int> freqMap;
+    for (char ch : data) {
+        freqMap[ch]++;
+    }
+    return freqMap;
+}               
--- HUFFMAN/Huffman.h
+++ HUFFMAN/Huffman.h
@@ -1,5 +1,23 @@
-#pragma once
-class Huffman
-{
-};
+#include <iostream>
+#include <fstream>
+#include <map>
+class Huffman {
+public:
 
+private:
+struct Node {
+        char symb;
+        int freq;
+        Node* left;
+        Node* right;
+
+        Node(char symb, int freq) : symb(symb), freq(freq), left(nullptr), right(nullptr) {}
+    };
+    struct Compare {
+        bool operator()(Node* left, Node* right) {
+            return left->freq > right->freq;
+        }
+    };
+    std::map<char, int> countFrequencies(const std::string& data);
+
+};   
