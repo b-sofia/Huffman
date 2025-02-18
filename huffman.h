2a833f4bc861942099bf00e8c729b40f56f68ef5
egortr2020 <136753951+egortr2020@users.noreply.github.com>
2025-02-18 10:45:05 +0300

    added countFrequencies, struct Node, struct Compare

---
+++ huffman.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+class Huffman {
+public:
+
+private:
+struct Node {
+        char symb;
+        int freq;
+        Node* left;
+        Node* right;
+
+        Node(char symb, int freq) : symb(symb), freq(freq), left(nullptr), right(nullptr) {}
+    };
+    struct Compare {
+        bool operator()(Node* left, Node* right) {
+            return left->freq > right->freq;
+        }
+    };
+    std::map<char, int> countFrequencies(const std::string& data);
+
+};
