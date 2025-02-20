f99025fdb3e01c4bdab1feba58c8b910997d1739
egortr2020 <136753951+egortr2020@users.noreply.github.com>
2025-02-20 10:10:52 +0300

    main added

--- HUFFMAN/main.cpp
+++ HUFFMAN/main.cpp
@@ -1,4 +1,11 @@
-int main()
-{
-	return 0;
+#include "huffman.h"
+
+int main() {
+    Huffman huffman;
+
+    huffman.compress("input.txt", "compressed.bin");
+
+    huffman.decompress("compressed.bin", "output.txt");
+
+    return 0;
 }
